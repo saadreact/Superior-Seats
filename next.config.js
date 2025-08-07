b852afa80a8655e0ccf5d122f03dc0f0ca2056ea
Syed Saad Shah <saadshah201@gmail.com>
2025-08-08 03:32:48 +0500

    Variations, Products Crud complete

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [],
+    domains: ['127.0.0.1', "https://superiorseats.ali-khalid.com"],
     unoptimized: true, // Allow all local images including those in subdirectories
   },
 }
